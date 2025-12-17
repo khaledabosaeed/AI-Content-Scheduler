5368f1b725e03fd08e833f44806928cd4c9b1f21
khaledabosaeed <kahledblata242@gmail.com>
2025-12-17 18:31:52 +0200

    move dashbord from user fetuers to widget

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 "use cline";
-import DashboardLayout from "@/features/user/dashboard/_components/DashboardLayout";
+import DashboardLayout from "@/widgets/dashboard/_components/DashboardLayout";
 import { use } from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <DashboardLayout>{children}</DashboardLayout>;
