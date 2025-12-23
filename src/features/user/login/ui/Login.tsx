e483749d5019997d1bdf9d58605e902b273b9b52
Razanbalata <razanbalata@gmail.com>
2025-12-23 21:39:28 +0200

    check build

--- src/features/user/login/ui/Login.tsx
+++ src/features/user/login/ui/Login.tsx
@@ -175,6 +175,7 @@ export default function StyledLoginForm() {
             <span className="flex-1 h-px bg-divider"></span>
           </div>
 
+
           {/* Social Button (Facebook) */}
           {/* <Button
             variant="outline"
